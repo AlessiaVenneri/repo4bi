519fdd0a66dc86fc5bdd6aec5c218b4287ab81f8
4b <4b@labinf1-01>
2023-03-04 12:10:46 +0100

    creazione metodo area 04/03/2023

--- src/Triangolo/triangolo.java
+++ src/Triangolo/triangolo.java
@@ -1,4 +1,30 @@
 package Triangolo;
 public class triangolo {
+	private float base;
+	private float altezza;
+	
+	public triangolo() {
+		
+	}
+	public float areatriangolo() {
+		return base*altezza;
+	}
+	
+	public float getBase() {
+		return base;
+	}
+	public void setBase(float base) {
+		this.base = base;
+	}
+	public float getAltezza() {
+		return altezza;
+	}
+	public void setAltezza(float altezza) {
+		this.altezza = altezza;
+	}
+
+	public String toString() {
+		return "triangolo dati: base=" + base + ", altezza=" + altezza;
+	}
 
 }
